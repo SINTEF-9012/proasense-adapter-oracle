0a1936eeb92fc8b0f1698635845b14c817309620
shakar70 <shakar70@gmail.com>
2015-09-14 09:34:07 +0200

    IMMAdapter with missing update from SQL.

--- oracle-base/src/main/java/net/modelbased/proasense/adapter/oracle/AbstractOracleAdapter.java
+++ oracle-base/src/main/java/net/modelbased/proasense/adapter/oracle/AbstractOracleAdapter.java
@@ -23,11 +23,10 @@ import eu.proasense.internal.VariableType;
 import net.modelbased.proasense.adapter.base.AbstractBaseAdapter;
 import org.apache.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
@@ -36,60 +35,38 @@ public abstract class AbstractOracleAdapter extends AbstractBaseAdapter {
     protected OracleConsumerInput inputPort;
 
     public final static Logger logger = Logger.getLogger(AbstractOracleAdapter.class);
-
-    public AbstractOracleAdapter() throws SQLException, ClassNotFoundException {
+    HashMap map = null;
+    ArrayList al = null;
+    public AbstractOracleAdapter() throws SQLException, ClassNotFoundException, InterruptedException {
         // Oracle input port properties
 
         String url = adapterProperties.getProperty("proasense.adapter.oracle.url");
         String username = adapterProperties.getProperty("proasense.adapter.oracle.username");
         String password = adapterProperties.getProperty("proasense.adapter.oracle.password");
-        String tableName = adapterProperties.getProperty("proasense.adapter.oracle.DBTableName1");
+        String globalTableName = adapterProperties.getProperty("proasense.adapter.oracle.DBTableName1");
         String sensor_id = adapterProperties.getProperty("proasense.adapter.base.sensorid");
         String reference_id_tags = adapterProperties.getProperty("proasense.adapter.oracle.imm.reference_id.tags");
+        String id_tags[] = reference_id_tags.split(",");
         String reference_id_mapping = adapterProperties.getProperty("proasense.adapter.oracle.imm.reference_id.mapping");
         String object_id_tag = adapterProperties.getProperty("proasense.adapter.oracle.imm.object_id.tags");
         String objectTag[] = object_id_tag.split(",");
         String object_id_mapping = adapterProperties.getProperty("proasense.adapter.oracle.imm.object_id.mapping");
         String objectValue[] = object_id_mapping.split(",");
 
-        HashMap map = createNameMap(objectTag, objectValue);
+        map = createNameMap(objectTag, objectValue);
+        al = refIdAndMapping(sensor_id, reference_id_mapping, id_tags);
         this.inputPort = new OracleConsumerInput(url, username, password);
         Connection con = inputPort.con;
-        java.sql.PreparedStatement statement = con.prepareStatement("select * from "+tableName);
-        ResultSet result = statement.executeQuery();
 
-        while (result.next()){
-           // convertToSimpleEvent(result);
-        if(map.containsKey(result.getString(4))){
-            String mappedVlues = (String) map.get(result.getString(4));
-            String splitValue[] = splitValues(mappedVlues);
-            long newDateObject = convertDate(result.getString(1));
-            String extractedValue = "";
-            ComplexValue complexValue = new ComplexValue();
+        checkForLatestTable(con, globalTableName);
 
-            if(splitValue[1].equals("STRING")){
-                extractedValue = result.getString(3);
-                complexValue.setType(VariableType.STRING);
-            }else if(splitValue[1].equals("DOUBLE")){
-                extractedValue = result.getString(7);
-                complexValue.setType(VariableType.DOUBLE);
-            }else if(splitValue[1].equals("INTEGER")){
-                extractedValue = result.getString(6);
-
-            }else if(splitValue[1].equals("CHAR")){
-
-                extractedValue = result.getString(5);
-            }
-            complexValue.setValue(extractedValue);
-            convertToSimpleEvent(sensor_id, newDateObject, splitValue[0],complexValue);
-            }
-        }
     }
 
     //hittil er strukturen det samme som for Montrac.
     //huske å bruke logget.debug("...");
-    protected abstract void convertToSimpleEvent(String sensorId, long timeStamp, String characteristic, ComplexValue complexValue);
+    protected abstract void convertToSimpleEvent(Connection con, HashMap map, ArrayList ref_id_mapping, String nameAndDate) throws SQLException, InterruptedException;
 
+    protected abstract void processTables(String sensorId, long timeStamp, String characteristic, ComplexValue complexValue) throws SQLException;
 
     HashMap createNameMap(String[] objectId, String[] nameAndValues){
         HashMap map = new HashMap();
@@ -100,12 +77,12 @@ public abstract class AbstractOracleAdapter extends AbstractBaseAdapter {
         return map;
     }
 
-    String[] splitValues(String valuesToSplit){
+    public String[] splitValues(String valuesToSplit){
             String[] splitTwoValues = valuesToSplit.split(":");
         return splitTwoValues;
     }
 
-    long convertDate(String date){
+    public long convertDate(String date){
         long timestamp = 0;
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-M-dd hh:mm:ss");
         try {
@@ -115,5 +92,40 @@ public abstract class AbstractOracleAdapter extends AbstractBaseAdapter {
             e.printStackTrace();
         }
         return timestamp;
+    }
+
+    public void  checkForLatestTable(Connection con, String globalTableName) throws SQLException, InterruptedException {
+        String newTableName = "'"+trimTableName(globalTableName)+"%'";
+        while(true) {
+
+            java.sql.PreparedStatement statement = con.prepareStatement("select * from(select object_name, created " +
+                    "from SYS.USER_OBJECTS where object_name like "+newTableName+" order by created desc)\n" +
+                    "  where rownum = 1");
+
+            ResultSet resultSet = statement.executeQuery();
+            if(resultSet.next()) convertToSimpleEvent(con, map, al, resultSet.getString(1)+","+resultSet.getString(2));
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("er i ytre while.");
+        }
+    }
+
+    ArrayList refIdAndMapping(String sensor_id, String refId, String[] refMapping){
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(0, sensor_id);
+        arrayList.add(1, refId);
+
+        for(int i = 0; i < refMapping.length; i++){
+            arrayList.add(i+2, refMapping[i]);
+        }
+        return arrayList;
+    }
+
+    String trimTableName(String nameFromProperties){
+        return nameFromProperties.substring(0, 5);
     }
 }
--- oracle-imm/src/main/java/net/modelbased/proasense/adapter/imm/IMMAdapter.java
+++ oracle-imm/src/main/java/net/modelbased/proasense/adapter/imm/IMMAdapter.java
@@ -23,12 +23,15 @@ import eu.proasense.internal.ComplexValue;
 import eu.proasense.internal.SimpleEvent;
 import eu.proasense.internal.VariableType;
 import net.modelbased.proasense.adapter.oracle.AbstractOracleAdapter;
+import org.joda.time.LocalDate;
 
 import java.lang.reflect.MalformedParameterizedTypeException;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -36,23 +39,59 @@ import java.util.Map;
 
 public class IMMAdapter extends AbstractOracleAdapter {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, InterruptedException {
     new IMMAdapter();
     }
 
-    public IMMAdapter() throws SQLException, ClassNotFoundException {
+    public IMMAdapter() throws SQLException, ClassNotFoundException, InterruptedException {
+        super();
 
     }
 
+    protected void convertToSimpleEvent(Connection con, HashMap map, ArrayList ref_id_mapping, String nameAndDate) throws SQLException, InterruptedException {
 
-    protected void convertToSimpleEvent(String sensorId, long timeStamp, String characteristic, ComplexValue complexValue){
+        LocalDate localDate = new LocalDate();
+        String[] tableNameAndDate = nameAndDate.split(",");
+        String creationDate[] = tableNameAndDate[1].split(" ");
+        String tableName = tableNameAndDate[0];
+        String sensor_id = ref_id_mapping.get(0).toString();
+        int newRowCount = 0;
+        int prevCount = 0;
+
+        while(true){
+            Thread.sleep(3000);
+            java.sql.PreparedStatement statement = con.prepareStatement("select count(*) from "+tableName);
+            ResultSet result = statement.executeQuery();
+
+
+            if(result.next())newRowCount = Integer.parseInt(result.getString(1));
+            if(creationDate[0].compareTo(localDate.toString()) == 0){
+                try {
+                    if(prevCount < newRowCount) createDelay(con, tableName, map, sensor_id, prevCount, newRowCount);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return;
+            }
+
+            try {
+                if(newRowCount > prevCount)createDelay(con, tableName, map, sensor_id, prevCount, newRowCount);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            prevCount = newRowCount;
+
+        }
+    }
+
+    protected void processTables(String sensorId, long timeStamp, String characteristic, ComplexValue complexValue){
 
         SimpleEvent simpleEvent = new SimpleEvent();
         simpleEvent.setSensorId(sensorId);
         simpleEvent.setTimestamp(timeStamp);
         simpleEvent.putToEventProperties(characteristic, complexValue);
         outputPort.publishSimpleEvent(simpleEvent);
-        System.out.println(simpleEvent.toString());
 
     }
 
@@ -64,6 +103,41 @@ public class IMMAdapter extends AbstractOracleAdapter {
         SimpleEvent event =  outputPort.createSimpleEvent(ref_id, timestamp, eventProperties);
         event.setSensorId("moulding");
         outputPort.publishSimpleEvent(event);
-        System.out.println(event.toString());
+    }
+
+    public void createDelay(Connection con, String tableName, HashMap map, String sensor_id,
+    int prevCount, int newCount) throws InterruptedException, SQLException {
+
+        java.sql.PreparedStatement statement = con.prepareStatement("SELECT *\n" +
+                " FROM(\n" +
+                "     SELECT ROW_NUMBER() OVER (ORDER BY CAPTURE_TS asc) AS rowNo, \n" +
+                "             capture_ts, CAPTURE_TOFF, REFERENCE_ID, OBJECT_ID, VALUE_CHAR, VALUE_INTEGER, VALUE_DECIMAL, VALUE_DATETIME\n" +
+                "             from "+tableName+")\n" +
+                "             where  rowNo BETWEEN "+prevCount+" and "+newCount);
+
+        ResultSet result = statement.executeQuery();
+
+        while (result.next()){
+            System.out.println(result.getString(5));
+            if(map.containsKey(result.getString(5))){
+
+                String mappedVlues = (String) map.get(result.getString(5));
+                String splitValue[] = splitValues(mappedVlues);
+                long newDateObject = convertDate(result.getString(2));
+                String extractedValue = "";
+                ComplexValue complexValue = new ComplexValue();
+
+                if(splitValue[1].equals("STRING")){
+                    extractedValue = result.getString(3);
+                    complexValue.setType(VariableType.STRING);
+                }else if(splitValue[1].equals("DOUBLE")){
+                    extractedValue = result.getString(7);
+                    complexValue.setType(VariableType.DOUBLE);
+                }
+
+                complexValue.setValue(extractedValue);
+                processTables(sensor_id, newDateObject, splitValue[0], complexValue);
+            }
+        }
     }
 }
